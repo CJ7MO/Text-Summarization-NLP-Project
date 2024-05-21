44654ae97dcf7a99abb9a7407199b2be2f876461
cj7mo <jcobando@uninorte.edu.co>
2024-05-20 22:24:42 -0500

    data validation added

--- src/textSummarizer/pipeline/stage_01_data_ingestion.py
+++ src/textSummarizer/pipeline/stage_01_data_ingestion.py
@@ -1,6 +1,5 @@
 from textSummarizer.config.configuration import ConfigurationManager
 from textSummarizer.components.data_ingestion import DataIngestion
-from textSummarizer import logger
 
 
 class DataIngestionTrainingPipeline:
