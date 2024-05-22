0e8b75f545660b56bba5c2f0d70fe736596a10e0
cj7mo <jcobando@uninorte.edu.co>
2024-05-22 15:33:33 -0500

    model trainer added

--- src/textSummarizer/components/data_transformation.py
+++ src/textSummarizer/components/data_transformation.py
@@ -11,18 +11,18 @@ class DataTransformation:
         self.tokenizer = AutoTokenizer.from_pretrained(config.tokenizer_name)
 
     def convert_examples_to_features(self, example_batch):
-        input_encodings = self.tokenizer(example_batch['dialogue'], max_length = 1024, truncation = True)
-
+        input_encodings = self.tokenizer(example_batch['dialogue'], truncation=True, padding='max_length',  max_length=512,)
+        
         with self.tokenizer.as_target_tokenizer():
-            target_encodings = self.tokenizer(example_batch['summary'], max_length= 128, truncation = True)
+            target_encodings = self.tokenizer(example_batch['summary'], truncation=True, padding='max_length', max_length=128,)
 
         return {
-            'input_ids' : input_encodings['input_ids'],
-            'attention_mask' : input_encodings['attention_mask'],
-            'labels' : input_encodings['input_ids']
+            'input_ids': input_encodings['input_ids'],
+            'attention_mask': input_encodings['attention_mask'],
+            'labels': target_encodings['input_ids']
         }
     
     def convert(self):
         dataset_samsum = load_from_disk(self.config.data_path)
         dataset_samsum_pt = dataset_samsum.map(self.convert_examples_to_features, batched=True)
-        dataset_samsum.save_to_disk(os.path.join(self.config.root_dir, "samsum_dataset"))
+        dataset_samsum_pt.save_to_disk(os.path.join(self.config.root_dir, "samsum_dataset"))
